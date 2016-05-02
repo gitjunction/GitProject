255ffbf2ab3edba311b1143946c7b3326e824dfd
GitJunction <gitjunction@gmail.com>
2016-05-02 12:49:05 +0530

    Conflict resolution Test

--- TestFile.java
+++ TestFile.java
@@ -5,7 +5,7 @@ public void test(){
 	System.out.println("Hello World");
 	System.out.println("Hello World Test");
 	System.out.println("Hello World Demo ");
-		System.out.println("Hello World Demo Test");
+		System.out.println("Hello World Demo Hello World");
 }
 
 }
